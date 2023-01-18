7f3c512f876410a1ce3f20dc8201608aeec01498
steph_cs <stecampelo@gmail.com>
2023-01-18 18:38:24 -0300

    componente principal

--- conversor-moedas/src/app/app.module.ts
+++ conversor-moedas/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { PaginaInicialComponent } from './template/pagina-inicial/pagina-inicial.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    PaginaInicialComponent
   ],
   imports: [
     BrowserModule,
