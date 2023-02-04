2338d38532c26f589d5d6ccaa48dc063913fd342
steph_cs <stecampelo@gmail.com>
2023-02-04 15:25:52 -0300

    test: servico historico conversao

---
+++ conversor-moedas/src/app/mock/MockHistoricoConversaoService.ts
@@ -0,0 +1,44 @@
+
+import { IHistoricoConversao } from 'src/app/interface/IHistoricoConversao';
+import { ISimbolos } from '../interface/ISimbolos';
+
+export class MockHistoricoConversaoService {
+
+  mockHistoricoConversao: IHistoricoConversao[] = [
+    {
+      data: "04/02/2023",
+      hora: "10:30:50",
+      valor: 150,
+      moedaOrigem: "BRL",
+      moedaDestino: "USD",
+      rate: 0.1942,
+      result: 29.129951,
+      altoValor: false
+    },
+    {
+      data: "04/02/2023",
+      hora: "10:30:50",
+      valor: 75000,
+      moedaOrigem: "BRL",
+      moedaDestino: "EUR",
+      rate: 0.179666,
+      result: 13474.938403,
+      altoValor: true
+    },
+    {
+      data: "04/02/2023",
+      hora: "10:30:50",
+      valor: 100000,
+      moedaOrigem: "BRL",
+      moedaDestino: "USD",
+      rate: 0.1942,
+      result: 19420.041021,
+      altoValor: true
+    }
+
+  ]
+  getHistorico(): IHistoricoConversao[] {
+    return this.mockHistoricoConversao
+  }
+
+}
--- conversor-moedas/src/app/service/historicoConversao/historico-conversao.service.spec.ts
+++ conversor-moedas/src/app/service/historicoConversao/historico-conversao.service.spec.ts
@@ -1,16 +1,72 @@
 import { TestBed } from '@angular/core/testing';
+import { IHistoricoConversao } from 'src/app/interface/IHistoricoConversao';
+import { MockHistoricoConversaoService } from 'src/app/mock/MockHistoricoConversaoService';
 
 import { HistoricoConversaoService } from './historico-conversao.service';
 
 describe('HistoricoConversaoService', () => {
   let service: HistoricoConversaoService;
+  let localStore: { [key: string]: string }
+  let mockHistorico: MockHistoricoConversaoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    localStore = {};
+
+    spyOn(window.localStorage, 'getItem').and.callFake((key) =>
+      key in localStore ? localStore[key] : null
+    );
+    spyOn(window.localStorage, 'setItem').and.callFake(
+      (key, value) => (localStore[key] = value)
+    );
+
     service = TestBed.inject(HistoricoConversaoService);
+    mockHistorico = new MockHistoricoConversaoService()
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should store the currency in localStorage', () => {
+    //add 1 conversao
+    service.addConversao(mockHistorico.mockHistoricoConversao[0]);
+
+    //recebe historico + push conversao 2
+    var historico: IHistoricoConversao[] = []
+    var json: any;
+
+    json = localStorage.getItem('historicoConversao')
+    historico = JSON.parse(json)
+
+    historico.push(mockHistorico.mockHistoricoConversao[1])
+    json = JSON.stringify(historico)
+    localStorage.setItem("historicoConversao", json)
+
+    json = JSON.stringify(mockHistorico.mockHistoricoConversao.slice(0,2))
+    expect(localStorage.getItem('historicoConversao')).toEqual(json);
+  });
+
+  it('should get currencies on localStorage', () => {
+    let json = JSON.stringify(mockHistorico.mockHistoricoConversao)
+    localStorage.setItem('historicoConversao', json);
+    expect(service.getHistorico()).toEqual(mockHistorico.mockHistoricoConversao);
+  });
+
+  it('should delete the currency in localStorage', () => {
+    let json = JSON.stringify([mockHistorico.mockHistoricoConversao[0]])
+    //add 1 conversao
+    service.addConversao(mockHistorico.mockHistoricoConversao[0]);
+    expect(localStorage.getItem('historicoConversao')).toEqual(json);
+    //del 1 conversao
+    service.delConversao(0)
+    expect(localStorage.getItem('historicoConversao')).toEqual('[]')
+  });
+
+  it('should clear the localStorage', () => {
+    service.delHistorico()
+    expect(localStorage.getItem('historicoConversao')).toBeNull()
+  });
+
+
 });
